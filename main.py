49f80568880954b03c9945566eb21e49c3c51287
jorgeabalo <131610142+jorgeabalo@users.noreply.github.com>
2026-09-13 17:18:09 -0500

    Procfile

--- main.py
+++ main.py
@@ -512,5 +512,7 @@ def chat_negocio(negocio_slug: str):
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    puerto = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=puerto)
